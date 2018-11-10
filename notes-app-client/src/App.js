22cc722acd3e0fc35540fcba6e969f97fad2d6ed
hariprasadkool <wowcricket1@gmail.com>
2018-11-11 00:41:04 +0530

    replaced render method in app.js page

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -32,7 +32,7 @@
 
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
@@ -42,10 +42,17 @@ class App extends Component {
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
-            <Navbar.Brand><Link to="/">Scratch</Link>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem href="/signup">Signup</NavItem>
+              <NavItem href="/login">Login</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
