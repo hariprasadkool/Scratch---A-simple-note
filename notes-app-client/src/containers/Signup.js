3864cb2b3af91d4347958ef2dc4f9a60944d587b
Ginnapu Hari Prasad <ginnapuharipvst@IN.nam.ad.pwcinternal.com>
2018-11-13 10:24:21 +0530

    adding or handling cognito signup

--- notes-app-client/src/containers/Signup.js
+++ notes-app-client/src/containers/Signup.js
@@ -7,6 +7,7 @@ import {
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import "./Signup.css";
+import { Auth } from "aws-amplify";
 export default class Signup extends Component {
     constructor(props) {
         super(props);
@@ -37,12 +38,32 @@ export default class Signup extends Component {
     handleSubmit = async event => {
         event.preventDefault();
         this.setState({ isLoading: true });
-        this.setState({ newUser: "test" });
+        try {
+            const newUser = await Auth.signUp({
+                username: this.state.email,
+                password: this.state.password
+            });
+            this.setState({
+                newUser
+            });
+        } catch (e) {
+            alert(e.message);
+        }
         this.setState({ isLoading: false });
     }
     handleConfirmationSubmit = async event => {
         event.preventDefault();
         this.setState({ isLoading: true });
+        try {
+            await Auth.confirmSignUp(this.state.email,
+                this.state.confirmationCode);
+            await Auth.signIn(this.state.email, this.state.password);
+            this.props.userHasAuthenticated(true);
+            this.props.history.push("/");
+        } catch (e) {
+            alert(e.message);
+            this.setState({ isLoading: false });
+        }
     }
     renderConfirmationForm() {
         return (
